b95fb8df7827f326fbbba83bef42dd974ae91411
Fluffy-Lion <leoncooper31@gmail.com>
2022-03-21 14:47:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [brew, setBrew] = useState("");
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+  const [input, setInput] = useState("")
+  let handleFetch = async () => {
+    try {
+      setError(false);
+      let response = await fetch("https://api.punkapi.com/v2/beers/random");
+      if (response.status !== 200) {
+        throw new Error("oops");
+      }
+      let data = await response.json();
+      setBrew(data[0]);
+    } catch (error) {
+      console.log("error: ", error);
+      setError({ error: true, message: error.message });
+    }
+  };
+  useEffect(() => {
+    handleFetch();
+    console.log("use effect worked");
+  }, []);
+  if(!brew ) {
+    return <h1>loading...</h1>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{brew.name}</h1>
+      {brew.food_pairing.map((item, index) => {
+        return <Food food={item} />;
+      })}
+      <button onClick={handleFetch}>fetch</button>
     </div>
   );
-}
+};
+
+const Food = ({ food }) => {
+  return (
+    <>
+      <h1>paired with...</h1>
+      <h2>{food}</h2>
+    </>
+  );
+};
 
 export default App;
